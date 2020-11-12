7a609132e1d7feea6ab1881e0195637b43764c7d
Michal Hozza <mhozza@gmail.com>
2020-11-12 21:04:34 +0000

    Configure publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.10"
+    `maven-publish`
 }
 
 group = "eu.hozza"
@@ -13,6 +14,25 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "ScrabblerKt"
+            url = uri("https://maven.pkg.github.com/mhozza/ScrabblerKt")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["kotlin"])
+            artifactId = "scrabbler"
+        }
+    }
 }
 
 tasks.jar {
